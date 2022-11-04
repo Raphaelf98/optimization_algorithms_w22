fix test gradient descent

assignments/a1_gradient_descent/test.py
@@ -41,7 +41,7 @@ class testGradientDescent(unittest.TestCase):
     def testBarrier(self):
 
         problem = NLPTraced(Barrier())
-        solution = np.zeros(0)
+        solution = np.zeros(2)
         x = solve(problem)
         self.assertTrue(np.linalg.norm(x-solution) < 1e-3)
 