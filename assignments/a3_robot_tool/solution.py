2338ecf49de4787f70a2f98e878ec7f579d0a947
raphael <r.ullrich.1@campus.tu-berlin.de>
2023-01-18 17:51:42 +0100

    solution to exercise a3_robot_tool

--- assignments/a3_robot_tool/solution.py
+++ assignments/a3_robot_tool/solution.py
@@ -34,7 +34,6 @@ class RobotTool(NLP):
         y[3] = np.sqrt(self.l)*(x[1] - self.q0[1])
         y[4] = np.sqrt(self.l)*(x[2] - self.q0[2])
         y[5] = np.sqrt(self.l)*(x[3] - self.q0[3])
-
         J = np.zeros((6,4))
         J[0, 0] = -np.sin(x[0]) - 0.5*np.sin(x[0]+x[1]) - (1/3+x[3])*np.sin(x[0]+x[1]+x[2])
         J[1, 0] = np.cos(x[0]) + 0.5*np.cos(x[0]+x[1]) + (1/3+x[3])*np.cos(x[0]+x[1]+x[2])
@@ -44,7 +43,6 @@ class RobotTool(NLP):
         J[1, 2] = (1/3+x[3])*np.cos(x[0]+x[1]+x[2])
         J[0, 3] = np.cos(x[0]+x[1]+x[2])
         J[1, 3] = np.sin(x[0]+x[1]+x[2])
-#
         J[2, 0] = np.sqrt(self.l)
         J[3, 1] = np.sqrt(self.l)
         J[4, 2] = np.sqrt(self.l)
